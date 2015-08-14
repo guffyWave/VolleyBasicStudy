fb312e9cbed1ff014f766d9afc0b0f553cfd0470
gufran <gufran.xpertsinfosoft@gmail.com>
2015-08-14 11:56:35 +0530

    Error Configuration Added

--- app/src/main/java/com/appxperts/volleystudy/MainActivity.java
+++ app/src/main/java/com/appxperts/volleystudy/MainActivity.java
@@ -8,9 +8,14 @@ import android.view.View;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Cache;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.StringRequest;
@@ -150,9 +155,21 @@ public class MainActivity extends ActionBarActivity {
                     System.out.println("        Headers : " + error.networkResponse.headers.toString());
                 }
 
+
+                if (error instanceof TimeoutError || error instanceof NoConnectionError) {
+                    System.out.println("TimeoutError " + error.getMessage());
+                } else if (error instanceof AuthFailureError) {
+                    System.out.println("AuthFailureError " + error.getMessage());
+                } else if (error instanceof ServerError) {
+                    System.out.println("ServerError " + error.getMessage());
+                } else if (error instanceof NetworkError) {
+                    System.out.println("NetworkError " + error.getMessage());
+                } else if (error instanceof ParseError) {
+                    System.out.println("ParseError " + error.getMessage());
+                }
+
             }
         });
-
 
 
         rq.add(st);
