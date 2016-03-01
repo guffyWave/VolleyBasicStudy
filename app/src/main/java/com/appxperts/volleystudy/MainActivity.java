3be705ea06f8e22fc98d010645165787a17e4aa1
Gufran Khurshid <gufran.khurshid@okutech.in>
2016-03-01 16:01:03 +0530

    Applied Volley File Upload

--- app/src/main/java/com/appxperts/volleystudy/MainActivity.java
+++ app/src/main/java/com/appxperts/volleystudy/MainActivity.java
@@ -1,35 +1,39 @@
 package com.appxperts.volleystudy;
 
-import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Cache;
-import com.android.volley.NetworkError;
-import com.android.volley.NoConnectionError;
-import com.android.volley.ParseError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Response;
-import com.android.volley.ServerError;
-import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
-    // Tag used to cancel the request
-    String tag_json_obj = "json_obj_req";
-    String url = "http://babesbabesapp.appspot.com/fetchbabesjson";
+public class MainActivity extends AppCompatActivity {
+
+    private final Context context = this;
+    private final String twoHyphens = "--";
+    private final String lineEnd = "\r\n";
+    private final String boundary = "apiclient-" + System.currentTimeMillis();
+    private final String mimeType = "multipart/form-data;boundary=" + boundary;
+    private byte[] multipartBody;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,169 +41,110 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    public void doWorkSecond(View v) {
+
+    }
+
+
     public void doWork(View v) {
 
-        Cache cache = VolleyStudyApplication.getInstance().getRequestQueue().getCache();
-        Cache.Entry entry = cache.get(url);
-        if (entry != null) {
-            try {
-                String data = new String(entry.data, "UTF-8");
-                // handle data, like converting it to xml, json, bitmap etc.,
-                Log.d(VolleyStudyApplication.TAG, "CACHED Data " + data);
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-                Log.d(VolleyStudyApplication.TAG, "UnsupportedEncodingException : " + e.toString()
-                );
-            }
-        } else {
-            // Cached response doesn't exists. Make network call here
-            talkToServer();
+        System.out.println("Started  File uploading .. . ");
+
+        byte[] fileData1 = null;
+
+        //fileData1 = getFileDataFromDrawable(context, R.mipmap.ic_launcher);
+
+        File file = new File(Environment.getExternalStorageDirectory() + "/Download" + "/happy.mp3");
+        int size = (int) file.length();
+        byte[] bytes = new byte[size];
+        fileData1 = bytes;
+        try {
+            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
+            buf.read(bytes, 0, bytes.length);
+            buf.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
 
-    private void talkToServer() {
-        // Tag used to cancel the request
-        String tag_string_req = "string_req";
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bos);
+        try {
+            //add the params
+            //buildTextPart(dos, "key", "value");
+            // the first file
+            buildPart(dos, fileData1, "audio_upload", "sound_test.3gpp");
+            // send multipart form data necessary after file data
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+            // pass to multipart body
+            multipartBody = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        final ProgressDialog pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Loading...");
-        pDialog.show();
+        String url = "http://mdev.broex.net/media/upload/audio";
 
-        StringRequest strReq = new StringRequest(Request.Method.POST,
-                url, new Response.Listener<String>() {
+        MultipartRequest multipartRequest = new MultipartRequest(url, null, mimeType, multipartBody, new Response.Listener<NetworkResponse>() {
             @Override
-            public void onResponse(String response) {
-                Log.d(VolleyStudyApplication.TAG, response.toString());
-                pDialog.hide();
+            public void onResponse(NetworkResponse response) {
+                String responseString = new String(response.data);
+                System.out.println("Sucess Response " + responseString);
+                Toast.makeText(context, "Upload successfully!", Toast.LENGTH_SHORT).show();
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                VolleyLog.d(VolleyStudyApplication.TAG, "Error: " + error.getMessage());
-                pDialog.hide();
-            }
-        }) {
-
-            /**
-             * Passing some request params
-             * */
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("name", "Androidhive");
-                params.put("email", "abc@androidhive.info");
-                params.put("password", "password123");
-
-                return params;
-            }
-
-            /**
-             * Passing some request headers
-             * */
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> headers = new HashMap<String, String>();
-                headers.put("Content-Type", "application/json");
-                headers.put("apiKey", "xxxxxxxxxxxxxxx");
-                return headers;
-            }
-        };
-
-// -------------->>Adding request to request queue
-        VolleyStudyApplication.getInstance().addToRequestQueue(strReq, tag_string_req);
-
-        VolleyStudyApplication.getInstance().getRequestQueue().getCache().invalidate(url, true);
-
-    }
-
-
-    public void useBasicVolley(View v) {
-
-        stringRequestWork();
-
-        //ImageRequest
-        //JsonRequest
-        //JsonArrayRequest
-        //JsonObjectRequest
-
-
-    }
-
-
-    private void stringRequestWork() {
-
-        RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
-        final ProgressDialog pd = new ProgressDialog(this);
-        pd.setMessage("Loading .. ");
-        pd.show();
-        //StringRequest----->>
-        StringRequest st = new StringRequest(Request.Method.GET, "http://gufranscifi.appspot.com/GetAllScienceFactJSON", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                System.out.println("Success Response " + response);
-                pd.hide();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                pd.hide();
-                System.out.println("Volley Error------>> ");
-                System.out.println("Message " + error.getMessage());
-                System.out.println("Network Time ms " + error.getNetworkTimeMs());
-                if (error.networkResponse != null) {
-                    System.out.println("    Network Response---- " + error.networkResponse.statusCode);
-                    System.out.println("        Status Code : " + error.networkResponse.statusCode);
-                    System.out.println("        NetworkTime ms : " + error.networkResponse.networkTimeMs);
-                    System.out.println("        Not Modified : " + error.networkResponse.notModified);
-                    System.out.println("        Data : " + error.networkResponse.data.toString());
-                    System.out.println("        Headers : " + error.networkResponse.headers.toString());
-                }
-
-
-                if (error instanceof TimeoutError || error instanceof NoConnectionError) {
-                    System.out.println("TimeoutError " + error.getMessage());
-                } else if (error instanceof AuthFailureError) {
-                    System.out.println("AuthFailureError " + error.getMessage());
-                } else if (error instanceof ServerError) {
-                    System.out.println("ServerError " + error.getMessage());
-                } else if (error instanceof NetworkError) {
-                    System.out.println("NetworkError " + error.getMessage());
-                } else if (error instanceof ParseError) {
-                    System.out.println("ParseError " + error.getMessage());
-                }
-
+                System.out.println("Error Response " + error);
+                Toast.makeText(context, "Upload failed!\r\n" + error.toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
+        VolleyStudyApplication.getInstance().addToRequestQueue(multipartRequest);
+    }
 
-        rq.add(st);
+    private void buildPart(DataOutputStream dataOutputStream, byte[] fileData, String fileKey, String fileName) throws IOException {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" + fileKey + "\"; filename=\""
+                + fileName + "\"" + lineEnd);
+        dataOutputStream.writeBytes(lineEnd);
+
+        ByteArrayInputStream fileInputStream = new ByteArrayInputStream(fileData);
+        int bytesAvailable = fileInputStream.available();
+
+        int maxBufferSize = 1024 * 1024;
+        int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+        byte[] buffer = new byte[bufferSize];
+
+        // read file and write it into form...
+        int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+        while (bytesRead > 0) {
+            dataOutputStream.write(buffer, 0, bufferSize);
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+        }
+
+        dataOutputStream.writeBytes(lineEnd);
     }
 
 
-    private void stringRequestWithParam() {
+    private byte[] getFileDataFromDrawable(Context context, int id) {
+        Drawable drawable = ContextCompat.getDrawable(context, id);
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, byteArrayOutputStream);
+        // InputStream in=new ByteArrayInputStream()
+        return byteArrayOutputStream.toByteArray();
+    }
 
-        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-        final ProgressDialog pd = new ProgressDialog(this);
-        pd.setMessage("Loading .. ");
-        pd.show();
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, "", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                return super.getParams();
-            }
-        };
-
-
+    private void buildTextPart(DataOutputStream dataOutputStream, String parameterName, String parameterValue) throws IOException {
+        dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+        dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" + parameterName + "\"" + lineEnd);
+        dataOutputStream.writeBytes("Content-Type: text/plain; charset=UTF-8" + lineEnd);
+        dataOutputStream.writeBytes(lineEnd);
+        dataOutputStream.writeBytes(parameterValue + lineEnd);
     }
 }
